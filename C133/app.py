30a6774206c3482108daf34e01665061abb3e3e1
Lucasbleyder2010 <100377923+Lucasbleyder2010@users.noreply.github.com>
2023-08-17 13:46:43 -0300

    Add files via upload

---
+++ C133/app.py
@@ -0,0 +1,54 @@
+# importando os módulos da biblioteca flask
+from flask import Flask , render_template
+
+# criando a instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+app = Flask(__name__)
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+@app.route("/")
+def home():
+
+    nome = "Lucas" # escreva seu nome
+    idade = "13" # escreva sua idade
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do pai
+
+@app.route("/pai")
+def father():
+
+    nome = "Derlei" # escreva seu nome
+    idade = "38" # escreva sua idade
+
+    return render_template('pai.html' , nome = nome , idade = idade)
+
+# defina a rota para a página da mãe
+
+@app.route("/mae")
+def mother():
+
+    nome = "Daniela" # escreva seu nome
+    idade = "37" # escreva sua idade
+
+    return render_template('mae.html' , nome = nome , idade = idade)
+
+
+# defina a rota para a página do amigo
+
+@app.route("/amigo")
+def friend():
+
+    nome = "Gabriel" # escreva seu nome
+    idade = "13" # escreva sua idade
+
+    return render_template('amigo.html' , nome = nome , idade = idade)
+
+
+# adicione outras rotas, se você quiser
+
+
+# execute o arquivo
+if __name__  ==  '__main__':
+    app.run(debug=True)
